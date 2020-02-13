aab0bc33537c1a3ce00f2891ae0e9ddb51ec3544
Baktiar Miah <baktiar.m@gmail.com>
2020-02-13 02:28:52 +0000

    Automatically backed up by Learn

--- db/migrate/20200213022834_add_artist_id_to_song.rb
+++ db/migrate/20200213022834_add_artist_id_to_song.rb
@@ -1,9 +1,5 @@
 class AddArtistIdToSong < ActiveRecord::Migration[5.0]
   def change
-    class AddArtistIdToSongs < ActiveRecord::Migration[5.0]
-  def change
     add_column :songs, :artist_id, :integer
-  end
-end
   end
 end
